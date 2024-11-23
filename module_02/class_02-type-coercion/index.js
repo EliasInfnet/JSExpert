6f24abaa78180f36b56f9804b6db7b3dc87e1feb
EliasInfnet <elias.carvalho@al.infnet.edu.br>
2024-11-23 16:47:28 -0300

    module02 class02-type-coercion

---
+++ module_02/class_02-type-coercion/index.js
@@ -0,0 +1,21 @@
+const item = {
+  name: "Elias",
+  age: 24,
+  toString() {
+    return `Name: ${this.name}, Age: ${this.age}`
+  },
+  valueOf() {
+    return 7
+  },
+  [Symbol.toPrimitive](coercionType) {
+    console.log("trying to convert to", coercionType)
+    const types = {
+      string: JSON.stringify(this),
+      number: "007",
+    }
+    return types[coercionType] || types.string
+  },
+}
+
+console.log(String(item))
+console.log(Number(item))
